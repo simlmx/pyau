90707e88cd676d49a85f2514200515c51ea8fd40
Douglas Eck <douglas.eck@gmail.com>
2009-12-10 13:24:28 -0500

    wrote tests for audio.py, added stuff for mandel repr in pamir

--- pyau/__init__.py
+++ pyau/__init__.py
@@ -1,2 +1,5 @@
-from pyaudiounit import *
+try :
+    from pyaudiounit import *
+except :
+    print "Audiounit not working on this machine.\nIf you received this while running setup.py test don't worry unless you need to use audiounit CoreAudio sound generation"
 import random_parameters
