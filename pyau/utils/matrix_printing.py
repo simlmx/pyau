47da0726116cb5dc976a0b391f74fa7c937bf407
Simon Lemieux <lemieux.simon@gmail.com>
2010-04-21 11:30:33 -0400

    nice ascii matrix printing in utils

---
+++ pyau/utils/matrix_printing.py
@@ -0,0 +1,30 @@
+#
+# Utils for printing matrices in the console
+# I use it for bad printing via ssh when I cannot use matplotlib!
+#
+# Simon Lemieux
+# april 20th 2010
+#
+
+from numpy import max, min
+
+def print_matrix(matrix, pixels=' .o0'):
+    """ Prints a matrix... """
+    max_ = max(matrix)
+    max_ = 1.0001*max_ # so we don't hit the max
+    min_ = min(matrix)
+    
+    matrix -= min_
+    matrix /= max_ - min_
+        
+    nb_pixels = len(pixels)
+    
+    for row in matrix:
+        for el in row:
+            idx = int(el*nb_pixels)
+            print pixels[idx],
+        print
+
+
+            
+    
